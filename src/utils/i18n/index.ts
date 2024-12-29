1c49fbd7ee1c0dd63846f1ecb8beeb66506c908a
mohnndZaghloul <mohanndzaghloul17@gmail.com>
2024-12-29 20:53:00 +0200

    minimize code

--- src/utils/i18n/index.ts
+++ src/utils/i18n/index.ts
@@ -2,11 +2,9 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import HttpApi from "i18next-http-backend";
-import Backend from "i18next-http-backend";
 
 i18n
   .use(initReactI18next)
-  .use(Backend)
   .use(LanguageDetector)
   .use(HttpApi)
   .init({
