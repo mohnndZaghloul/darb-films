0cbf03a1567086f83f645b3dc5595610ad4f5e90
mohnndZaghloul <mohanndzaghloul17@gmail.com>
2024-12-29 20:42:00 +0200

    update lang and work

--- src/utils/i18n/index.ts
+++ src/utils/i18n/index.ts
@@ -4,31 +4,13 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import HttpApi from "i18next-http-backend";
 import Backend from "i18next-http-backend";
 
-// import enLang from "../../public/locales/en.json";
-// import arLang from "../../public/locales/ar.json";
-
-// const resources = {
-//   en: {
-//     translation: {
-//       welcomeMessage: enLang,
-//     },
-//   },
-//   ar: {
-//     translation: {
-//       welcomeMessage: arLang,
-//     },
-//   },
-// };
-
 i18n
   .use(initReactI18next)
   .use(Backend)
   .use(LanguageDetector)
   .use(HttpApi)
   .init({
-    // resources,
     fallbackLng: "ar",
-    lng: "en",
     debug: true,
     detection: {
       order: [
