bf1ba975dbe6ef76f4cee206f237b2988b1fec87
mohnndZaghloul <mohanndzaghloul17@gmail.com>
2024-12-31 12:10:17 +0200

    animation

--- src/components/templates/Portfolio.tsx
+++ src/components/templates/Portfolio.tsx
@@ -13,57 +13,57 @@ const Portfolio = () => {
 
   return (
     <section className="container grid sm:grid-cols-2 lg:grid-cols-3 gap-x-6 my-16">
-      <div className="my-14">
+      <div className="my-14 group cursor-pointer">
         <div className="relative w-full">
           <img className="w-full" src={PortfolioImg3} alt="image" />
-          <div className="absolute bottom-0 w-full h-1/2 bg-gradient-to-t from-black" />
+          <div className="absolute bottom-0 w-full h-1/2 bg-gradient-to-t from-black translate-y-full group-hover:translate-y-0 transition duration-500" />
         </div>
-        <h3 className="font-bold text-xl text-center py-4">
+        <h3 className="group-hover:opacity-100 opacity-0 transition duration-300 font-bold text-xl text-center py-4">
           {t("companyVideo")}
         </h3>
       </div>
-      <div className="my-14">
+      <div className="my-14 group cursor-pointer">
         <div className="relative w-full">
           <img className="w-full" src={PortfolioImg2} alt="image" />
-          <div className="absolute bottom-0 w-full h-1/2 bg-gradient-to-t from-black" />
+          <div className="absolute bottom-0 w-full h-1/2 bg-gradient-to-t from-black translate-y-full group-hover:translate-y-0 transition duration-500" />
         </div>
-        <h3 className="font-bold text-xl text-center py-4">
+        <h3 className="group-hover:opacity-100 opacity-0 transition duration-300 font-bold text-xl text-center py-4">
           {t("portraitPhotography")}
         </h3>
       </div>
-      <div className="my-14">
+      <div className="my-14 group cursor-pointer">
         <div className="relative w-full">
           <img className="w-full" src={PortfolioImg1} alt="image" />
-          <div className="absolute bottom-0 w-full h-1/2 bg-gradient-to-t from-black" />
+          <div className="absolute bottom-0 w-full h-1/2 bg-gradient-to-t from-black translate-y-full group-hover:translate-y-0 transition duration-500" />
         </div>
-        <h3 className="font-bold text-xl text-center py-4">
+        <h3 className="group-hover:opacity-100 opacity-0 transition duration-300  font-bold text-xl text-center py-4">
           {t("eventPhotography")}
         </h3>
       </div>
-      <div className="my-14">
+      <div className="my-14 group cursor-pointer">
         <div className="relative w-full">
           <img className="w-full" src={PortfolioImg6} alt="image" />
-          <div className="absolute bottom-0 w-full h-1/2 bg-gradient-to-t from-black" />
+          <div className="absolute bottom-0 w-full h-1/2 bg-gradient-to-t from-black translate-y-full group-hover:translate-y-0 transition duration-500" />
         </div>
-        <h3 className="font-bold text-xl text-center py-4">
+        <h3 className="group-hover:opacity-100 opacity-0 transition duration-300 font-bold text-xl text-center py-4">
           {t("aerialPhotography")}
         </h3>
       </div>
-      <div className="my-14">
+      <div className="my-14 group cursor-pointer">
         <div className="relative w-full">
           <img className="w-full" src={PortfolioImg5} alt="image" />
-          <div className="absolute bottom-0 w-full h-1/2 bg-gradient-to-t from-black" />
+          <div className="absolute bottom-0 w-full h-1/2 bg-gradient-to-t from-black translate-y-full group-hover:translate-y-0 transition duration-500" />
         </div>
-        <h3 className="font-bold text-xl text-center py-4">
+        <h3 className="group-hover:opacity-100 opacity-0 transition duration-300 font-bold text-xl text-center py-4">
           {t("podcastProduction")}
         </h3>
       </div>
-      <div className="my-14">
+      <div className="my-14 group cursor-pointer">
         <div className="relative w-full">
           <img className="w-full" src={PortfolioImg4} alt="image" />
-          <div className="absolute bottom-0 w-full h-1/2 bg-gradient-to-t from-black" />
+          <div className="absolute bottom-0 w-full h-1/2 bg-gradient-to-t from-black translate-y-full group-hover:translate-y-0 transition duration-500" />
         </div>
-        <h3 className="font-bold text-xl text-center py-4">
+        <h3 className="group-hover:opacity-100 opacity-0 transition duration-300 font-bold text-xl text-center py-4">
           {t("corporateMediaLibrary")}
         </h3>
       </div>
